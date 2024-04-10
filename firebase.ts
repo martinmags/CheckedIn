26117f9a7eb30a2446e56cebc9cec89967db4cc1
Martin Magsombol <martinmagsombol@Martins-Air.localdomain>
2024-04-10 10:54:59 -0700

    setup firebase

--- firebase.ts
+++ firebase.ts
@@ -1,19 +1,27 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import Config from "react-native-config";
+import {initializeApp, getApps, getApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+import Config from 'react-native-config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: Config.FIREBASE_APP_KEY,
-    authDomain: Config.FIREBASE_AUTH_DOMAIN,
-    projectId: Config.FIREBASE_PROJECT_ID,
-    storageBucket: Config.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
-    appId: Config.FIREBASE_APP_ID,
-    measurementId: Config.FIREBASE_MEASUREMENT_ID
-  };
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID,
+  measurementId: Config.FIREBASE_MEASUREMENT_ID,
+};
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
-const auth = getAuth(app);
-export { auth, app, db }
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export {auth, app, db, getAuth};
--- screens/LoginScreen/LoginScreen.tsx
+++ screens/LoginScreen/LoginScreen.tsx
@@ -1,30 +1,9 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import {GoogleAuthProvider, signInWithCredential} from 'firebase/auth';
-import {auth} from '../../firebase';
-import Config from 'react-native-config';
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-} from '@react-native-google-signin/google-signin';
+import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
 import styles from './LoginScreen.styles.ts';
 
-GoogleSignin.configure({
-  webClientId: Config.GOOGLE_SIGN_IN_CLIENT_ID,
-});
-
-const LoginScreen = () => {
-  const googleSignIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const {idToken}: any = await GoogleSignin.signIn();
-      const googleCredentials = GoogleAuthProvider.credential(idToken);
-      await signInWithCredential(auth, googleCredentials);
-    } catch (error: any) {
-      console.log(error);
-    }
-  };
-
+const LoginScreen = ({googleSignIn}: {googleSignIn: any}) => {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}></View>
